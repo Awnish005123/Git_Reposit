7ad10340e838146d2f3bdea80309eaff15e8d1e2
Awnish005123 <147350801+Awnish005123@users.noreply.github.com>
2024-09-03 16:47:33 -0400

    Adding dataset (#10)
    * two hand Images in the dataset removed.
    
    * Change in the dataset-Two hand images removed
    
    ---------
    
    Co-authored-by: Awnish005123 <awnishgagan005@gmail.com>

--- Create_Dataset.py
+++ Create_Dataset.py
@@ -64,14 +64,14 @@ for a in labels:
     if a in symbol_dict.keys():
         Labels_num.append(symbol_dict[a])
 
-f = open('dataset.pickle', 'wb') # wb stands for write binary.
+f = open('sl_dataset.pickle', 'wb') # wb stands for write binary.
 pickle.dump({'data': data, 'labels': Labels_num}, f)
 f.close()
 
 ## Exploring the dataset. Not related to very relevant to the project.
 
 # Open the file in binary read mode
-with open('dataset.pickle', 'rb') as f: # rb here is read binary.
+with open('sl_dataset.pickle', 'rb') as f: # rb here is read binary.
     # Load the data from the file
     dataset = pickle.load(f)
 
@@ -82,12 +82,19 @@ for key,values in dataset.items():
 # Print the data to see its contents
 print(df.head(10))
 
-list_length = [len(sub_list) for sub_list in df['data']]
-Min_length = max(list_length)
-Max_length = max(list_length)
+# one row comprises a single list, it will give csv in which the list fits in one cell.
+df.to_csv('sl_dataset.csv',index=False )
 
-for i in list_length:
-    if i != 42 :
-        print(i)
+# Create a DataFrame where each element in the list becomes its own column
+df_features = pd.DataFrame(df['data'].tolist(), index=df.index) # index will add index to col.
+print(df_features.head(10))
 
+# Concatenate the new DataFrame with the output column
+df_final = pd.concat([df_features, df['labels']], axis=1)
+
+## Print the data to see its contents
+print(df_final.head(10))
+
+## one row comprises a single list, it will give csv in which the list fits in one cell.
+df.to_csv('sl_dataset_final.csv',index=False )
 
